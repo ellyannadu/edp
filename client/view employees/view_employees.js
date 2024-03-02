16acc8221a7fb1057a2383e68a08e8c14f3c8f70
Arj <jraptabudlong@addu.edu.ph>
2024-03-02 22:56:27 +0800

    added modal for add/update. removed sidbare.

--- client/view employees/view_employees.js
+++ client/view employees/view_employees.js
@@ -1,26 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var modal = document.getElementById("myModal");
-    var infoButtons = document.querySelectorAll(".info-button");
-    var span = document.getElementsByClassName("close")[0];
+    var modal = document.querySelectorAll(".modal");
+    var modalBtns = document.querySelectorAll(".modalBtn");
+    var close = document.querySelectorAll(".close");
 
-    infoButtons.forEach(function(button) {
-        button.onclick = function() {
-            modal.style.display = "block";
-            populateModal(this);
+    modalBtns.forEach(function(btn, index) {
+        btn.onclick = function() {
+            modal[index].style.display = "block";
+            populateModal();
         }
     });
 
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    window.onclick = function(event) {
-        if (event.target == modal) {
+    close.forEach(function(btn) {
+        btn.onclick = function() {
+            // Close the modal associated with this close button
+            var modal = btn.parentElement.parentElement;
             modal.style.display = "none";
-        }
-    }
+          };
+        });
 
-    function populateModal(clickedButton) {
+        window.onclick = function(event) {
+            modals.forEach(function(modal) {
+              if (event.target == modal) {
+                modal.style.display = "none";
+              }
+            });
+          };
+
+
+    function populateModal() {
         var employeeData = {
             id: "1",
             empNo: "111",
@@ -35,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function() {
             city: "Davao",
             province: "Davao del Sur",
             zipCode: "8000",
-            actions: '<div class="button-container">' + '<button class="edit-button"><i class="fa-regular fa-pen-to-square"></i></button>' + '<button class="delete-button"><i class="fa-solid fa-trash"></i></button>' + '</div>',
+            actions: '<div class="button-container">' + '<button id="updateBtn" class="modalBtn"><i class="fa-regular fa-pen-to-square"></i></button>' + '<button class="modalBtn"><i class="fa-solid fa-trash"></i></button>' + '</div>',
         };
 
         var modalBody = document.getElementById("employee-modal-body");
@@ -56,6 +63,24 @@ document.addEventListener("DOMContentLoaded", function() {
                 <td>${employeeData.zipCode}</td>
                 <td>${employeeData.actions}</td>
             </tr>
-        `;
+            `;
+
+    // Get the modal
+    var updateModal = document.getElementById('updateModal');
+
+    // Add event listener to the document
+    document.addEventListener('click', function(event) {
+        // If the clicked element is the updateBtn, display the updateModal
+        if (event.target.id === 'updateBtn') {
+            updateModal.style.display = 'block';
+        }
+    });
     }
+
+    
+
+           
+
+            
+        
 });
