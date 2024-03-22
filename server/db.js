ec81ac07e25f760360df41341a1f0a981a8d0c68
Arj <jraptabudlong@addu.edu.ph>
2024-03-22 21:29:30 +0800

    password

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const pool = new Pool({
     user: process.env.DB_USER, 
-    password: 'curbstomp',
+    password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME
